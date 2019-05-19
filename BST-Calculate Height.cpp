6d01b9c96161753e64695afe5bd550147a3da841
pisawhne <pisawhne@cisco.com>
2019-05-19 13:27:21 +0530

    v1

---
+++ BST-Calculate Height.cpp
@@ -0,0 +1,59 @@
+// Finding height of the BST
+#include <iostream>
+using namespace std;
+
+struct node{
+    int data;
+    struct node* left;
+    struct node* right;
+};
+
+node* root = nullptr;
+node* GetnewNode(int data);
+node* Insert(node* root, int data);
+int height(node* root);
+
+int main(){
+    root = Insert(root,15);
+    root = Insert(root,10);
+    root = Insert(root,20);
+    root = Insert(root,8);
+    root = Insert(root,12);
+    root = Insert(root,17);
+    root = Insert(root,25);
+    root = Insert(root,11);
+    root = Insert(root,9);
+
+    cout<<height(root);
+    return 0;
+}
+
+node* GetnewNode(int data){
+    node* newNode = new node();
+    newNode->data = data;
+    newNode->left = nullptr;
+    newNode->right = nullptr;
+    return  newNode;
+}
+
+node* Insert(node* root_ptr, int data){
+    if(root_ptr == nullptr){
+        root_ptr = GetnewNode(data);
+    }
+    else if(root_ptr->data<data){
+        root_ptr->right = Insert(root_ptr->right,data);
+    }
+    else if(root_ptr->data>=data){
+        root_ptr->left = Insert(root_ptr->left,data);
+    }
+    return root_ptr;
+}
+
+int height(node* root_ptr){
+    if(root_ptr == nullptr){
+        return -1;
+    }
+    else{
+        return max(height(root_ptr->left),height(root_ptr->right)) + 1;
+    }
+}
